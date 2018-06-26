3bf17d94d5223a6f4eebee71b8e01f42f9b6c3b8
Jaz11 <jahzell111@gmail.com>
2018-06-26 09:05:15 -0500

    new update

---
+++ src/data.js
@@ -0,0 +1,23 @@
+window.computeUsersStats = () => {
+ 
+}
+
+window.sortUsers = () => {
+
+}
+
+window.filterUsers = () => {
+
+}
+
+window.processCohortData = () => {
+
+}
+
+
+//Buttons
+const button2018Function = 
+document.getElementById('prom-2018-button').addEventListener('click', () => {
+document.getElementById('block-2').style.display = 'none';
+document.getElementById('block-3').style.display = 'block';
+});
