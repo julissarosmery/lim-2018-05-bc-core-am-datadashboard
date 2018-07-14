348c3667d2c2d47abbee55e2f7c29b5e215f1dd0
julissa <rosmery0239@gmail.com>
2018-07-14 13:28:45 -0500

    actualizando develop

--- src/data.js
+++ src/data.js
@@ -1,41 +1,62 @@
 window.computeUsersStats=(users,progress,courses)=>{
-  const datosUsuarias = users;
-  const datosProgresos = progress;
-  
-  const calculandoStadisticas=(usuaria,typo)=>{
-  
-  }
-  //filtar usuarias :
-  let students = datosUsuarias.filter(usu=>usu.role ===  "student");
-  // console.log(students);
-  //mapeando usuarias:
-  students = students.map(usu=>{ 
-  const usuariasProgreso=datosProgresos[usu.id];
-  console.log(usuariasProgreso);
-  let percent = calculandoPorcentaje(usuariasProgreso);
-  let execises =  calculandoStadisticas(usuariasProgreso,"practice");
-  let reads = calculandoStadisticas(usuariasProgreso,"reads");
-  let quizzes = calculandoStadisticas(usuariasProgreso,"quizzes");
-  
-  })
-  
-  
-  
-  
-  }
-  window.sortUsers=(users,orderBy,orderDirection)=>{
-  
-  }
-  window.filterUsers=(users,search)=>{
-  
-  }
-  window.processCohortData = (options) => {
-      // console.log(options);
-  const courses=Object.keys(options.cohort.coursesIndex);
-  // console.log(courses);
-      
-      let students = computeUsersStats(options.cohortData.users,options.cohortData.progress,courses);
-      // students=sortUsers(users,orderBy,orderDirection);
-      // search ? students=filterUsers(users,search):null;
-      // console.log(options);
-  }
+    const datosUsuarias = users;
+    const datosProgresos = progress;
+    
+    const calcularPorcentaje = user =>{
+    let contador=0;
+    let porcentajeDeCursos=[];
+    courses.map(curso => {
+    if(user [curso]){
+    
+    
+         
+    }
+    })
+    }
+    
+    const calculandoStadisticas=(usuaria,typo)=>{
+    
+    }
+    //filtar usuarias :
+    let students = datosUsuarias.filter(usu=>usu.role === "student");
+    // console.log(students);
+    //mapeando usuarias:
+    students = students.map(usu=>{ 
+        //vinculamos usuarios y progreso por el id . que es lo que ambos tienen:
+    const usuariasProgreso = datosProgresos[usu.id];
+    console.log(usuariasProgreso);
+    let percent = calculandoPorcentaje(usuariasProgreso);
+    let execises =  calculandoStadisticas(usuariasProgreso,"practice");
+    let reads = calculandoStadisticas(usuariasProgreso,"reads");
+    let quizzes = calculandoStadisticas(usuariasProgreso,"quizzes");
+    return ({
+    //     id:usu.id,
+    //     name:usu.name.toUpperCase(),
+    
+        stats:{
+            percent:percent,
+            exercises:excercises,
+            reads:reads,
+            quizzes:quizzes
+        },
+    })
+    });
+    return students;
+    }
+    
+    window.sortUsers=(users,orderBy,orderDirection)=>{
+    
+    }
+    window.filterUsers=(users,search)=>{
+    
+    }
+    window.processCohortData = (options) => {
+        // console.log(options);
+    const courses=Object.keys(options.cohort.coursesIndex);
+    // console.log(courses);
+        
+        let students = computeUsersStats(options.cohortData.users,options.cohortData.progress,courses);
+        // students=sortUsers(users,orderBy,orderDirection);
+        // search ? students=filterUsers(users,search):null;
+        // console.log(options);
+    }
--- src/main.js
+++ src/main.js
@@ -45,6 +45,7 @@ const error = () => {
 const addCohorts = (sedesHtml, datacohorts) => {
   // console.log(sedesHtml , datacohorts);
   options.cohort = datacohorts;
+  // console.log(options);
 //   // usersmostrar.innerHTML = '';
   const datosCohortsFiltrados = datacohorts.filter(fCohort => {
     return fCohort.id.indexOf(sedesHtml) !== -1;
@@ -68,7 +69,7 @@ const addProgress = (usersName, dataProgress) => {
   // console.log(usersName, dataProgress);
   options.cohortData.progress = dataProgress;
   // console.log(options);
-//   // processCohortData(options);
+  processCohortData(options);
 };
 
 
@@ -112,13 +113,116 @@ sedes.addEventListener('click', (event) => {
 })
 cohortsMostrar.addEventListener('click', (event) => {
 
-   const cohortSeleccionado = options.cohort.filter(element => {
-   if (element.id === "lim-2018-03-pre-core-pw"){
-     options.cohort = element.id;
-     
-   }
-  });
-  options.cohort(cohortSeleccionado[0]);
+  const cohortSeleccionado =  options.cohort.filter(ele => {
+    if(ele.id === "lim-2018-03-pre-core-pw"){
+      return ( ele.id);
+      options.cohort = ele.id;
+    }
+  })
+
+  console.log(cohortSeleccionado[0]);
+  
+  options.cohort = cohortSeleccionado[0]
+// console.log(options);
   obtJson(event.target.id, urlUser, addUsers);
 
 })
+
+
+// const sedes = document.querySelector('#sedes');
+// // const cohort = document.querySelector('#cohorts');
+
+
+
+// const urlCohorts = "../data/cohorts.json";
+// const urlUser = '../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+// const urlProgress = "../data/cohorts/lim-2018-03-pre-core-pw/progress.json";
+
+// const cohortsMostrar = document.getElementById('mostrarCohorts');
+// const usersmostrar = document.getElementById('mostrarUsers');
+
+// const options = {
+//   cohort: null,
+//   cohortData: {
+//     users: null,
+//     progress: null,
+//   },
+//   orderBy: 'name',
+//   orderDirection: 'ASC',
+//   search: ''
+// };
+
+
+
+// // HY ORDER FUNCION PARA SOLICITAR DATOS:
+// const obtJson = (str, url, llamarDenuevo) => {
+//   const solicitud = new XMLHttpRequest();
+//   solicitud.open('GET', url, true);
+//   solicitud.addEventListener('load', event => {
+//     if (event.target.readyState === 4 && event.target.status === 200) {
+//       const response = (JSON.parse(event.target.responseText));
+//       llamarDenuevo(str, response)
+//     }
+//   });
+//   solicitud.onerror = error;
+//   solicitud.send();
+// }
+// const error = () => {
+//   console.log("oh my god que es estoooo..a ocurrido un error!!");
+// }
+
+
+// //FUNCION PARA LLAMAR ACOHORTS:
+// const addCohorts = (sedesHtml, datacohorts) => {
+//   options.cohort = datacohorts;
+//   const datosCohortsFiltrados = datacohorts.filter(fCohort => {
+//     return fCohort.id.indexOf(sedesHtml) !== -1;
+//   });
+//   cohortsMostrar.innerHTML = '';
+//   for (const fCohort of datosCohortsFiltrados) {
+//     cohortsMostrar.innerHTML+='';
+
+//     let listCohorts = document.createElement('div');
+//     listCohorts.setAttribute('class', 'chau');
+//     listCohorts.textContent = fCohort.id;
+//     cohortsMostrar.appendChild(listCohorts);
+
+
+//   }
+
+// }
+// // //FUNCION PARA LLAMAR A PROGRESS:
+// const addProgress = (usersName, dataProgress) => {
+//   options.cohortData.progress = dataProgress;
+// //   // processCohortData(options);
+// };
+
+
+// // // FUNCION PARA LLLAMAR A LOS USERS:    
+// const addUsers = (usersName, dataUsers) => {
+//   options.cohortData.users = dataUsers;
+//   obtJson(usersName, urlProgress, addProgress);
+
+// }
+
+// sedes.addEventListener('click', (event) => {
+
+//   obtJson(event.target.id, urlCohorts, addCohorts);
+
+
+// })
+// cohortsMostrar.addEventListener('click', (event) => {
+
+//    options.cohort.forEach(element => {
+//    if (element.id === event.target.id){
+//      options.cohort = element;
+     
+//    }
+  
+
+//   });
+//   console.log(options);
+  
+//   obtJson(event.target.id, urlUser, addUsers);
+
+// })
